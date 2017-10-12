a41f09462571b3bbe6486bbbc174e2c1b966b11a
cloud.hao <cloud.hao@huoguiwell.com>
2017-10-12 15:20:09 +0800

    初始提交

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<<template>
+  <div class="main">
+    <img class="loginImg" src="../../static/image/bg_login.jpg">
+  </div>
+</template>
+<style>
+  .main{
+    margin: 0px;
+    padding: 0px;
+    width: 100%;
+    height: 100%;
+  }
+  .loginImg{
+    margin: 0px;
+    padding: 0px;
+    width: 100%;
+    height: 100%;
+  }
+</style>
