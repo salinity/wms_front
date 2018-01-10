f81a20843d37f91d7a8549e67ce7edc2cd8afead
cloud.hao <cloud.hao@huoguiwell.com>
2018-01-10 11:00:29 +0800

    commit update

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,19 @@
 <template>
   <div id="app">
-    <el-container>
-      <el-header>
-        <layouts-header>Header</layouts-header>
-      </el-header>
-      <el-container>
-        <el-aside>
-          <layouts-aside width="200px">Aside</layouts-aside>
-        </el-aside>
         <el-container>
           <el-main>
             <layouts-main></layouts-main>
           </el-main>
-          <el-footer>
-            <layouts-footer></layouts-footer>
-          </el-footer>
         </el-container>
-      </el-container>
-    </el-container>
   </div>
 </template>
 
 <script>
-import layoutsHeader from './components/layouts/Header'
-import layoutsAside from './components/layouts/Aside'
 import layoutsMain from './components/layouts/Main'
-import layoutsFooter from './components/layouts/Footer'
 export default {
   name: 'app',
   components: {
-    layoutsHeader,
-    layoutsAside,
-    layoutsMain,
-    layoutsFooter
+    layoutsMain
   },
   beforeCreate: function () {
     console.log('App beforeCreate 初始化时同步调用')
@@ -63,47 +44,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-.el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-  }
-  
-  .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
-  }
-  
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-  }
-  
-  body > .el-container {
-    margin-bottom: 40px;
-  }
-  
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  }
-  
-  .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-  }
-
-</style>
--- src/components/layouts/Main.vue
+++ src/components/layouts/Main.vue
@@ -1,3 +1,68 @@
-<<template>
-  <router-view></router-view>
+<template>
+<div id="main">
+  <el-container>
+    <el-header><layouts-header></layouts-header></el-header>
+    <el-container>
+      <el-aside><layouts-aside></layouts-aside></el-aside>
+      <router-view></router-view>
+    </el-container>
+    <el-footer><layouts-footer></layouts-footer></el-footer>
+  </el-container>
+</div>
 </template>
+<script>
+import layoutsHeader from './Header'
+import layoutsAside from './Aside'
+import layoutsFooter from './Footer'
+export default {
+  name: 'main',
+  components: {
+    layoutsHeader,
+    layoutsAside,
+    layoutsFooter
+  }
+}
+</script>
+<style>
+#main {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+
+</style>
